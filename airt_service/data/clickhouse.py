3aa68f87eb2bbc172749865712c407995cb00103
Kumaran Rajendhiran <kumaran@airt.ai>
2023-03-30 13:40:00 +0000

    Get only distinct person ids

--- airt_service/data/clickhouse.py
+++ airt_service/data/clickhouse.py
@@ -778,7 +778,7 @@ def _get_all_person_ids_for_account_ids(
 
         account_ids_query = ", ".join([str(a_id) for a_id in account_ids])
         # nosemgrep: python.sqlalchemy.security.sqlalchemy-execute-raw-query.sqlalchemy-execute-raw-query
-        query = f"SELECT AccountId, PersonId FROM {database}.{table} WHERE AccountId IN ({account_ids_query}) ORDER BY PersonId ASC"  # nosec B608
+        query = f"SELECT DISTINCT PersonId, AccountId FROM {database}.{table} WHERE AccountId IN ({account_ids_query}) ORDER BY PersonId ASC"  # nosec B608
         logger.info(f"Getting count with query={query}")
 
         df = pd.read_sql(sql=query, con=connection)
